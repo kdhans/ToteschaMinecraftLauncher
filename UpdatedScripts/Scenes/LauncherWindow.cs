d17ad7c6879d5b4a14c47a425c8e3a6dff64c97c
kdhans <43083751+kdhans@users.noreply.github.com>
2024-10-08 15:29:09 -0700

    10/8 partial update

--- UpdatedScripts/Scenes/LauncherWindow.cs
+++ UpdatedScripts/Scenes/LauncherWindow.cs
@@ -1,69 +1,126 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Godot;
-//using ToteschaMinecraftLauncher.UpdatedScripts.Controllers;
-//using ToteschaMinecraftLauncher.UpdatedScripts.Contracts;
-//using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+using ToteschaMinecraftLauncher.UpdatedScripts.Controllers;
+using ToteschaMinecraftLauncher.UpdatedScripts.Contracts;
+using System.IO;
 
-//namespace ToteschaMinecraftLauncher.UpdatedScripts.Scenes
-//{
-//    public partial class LauncherWindow : Control
-//    {
-//        private readonly SettingsController _settingsController = new SettingsController();
-//        private const int MinimumWidth = 1400;
-//        private const int MinimumHeight = 750;
-//        private string? _settingsDirectory;
+namespace ToteschaMinecraftLauncher.UpdatedScripts.Scenes
+{
+	public partial class LauncherWindow : Control
+	{
+		private readonly SettingsController _settingsController = new SettingsController();
+		private const int MinimumWidth = 1800;
+		private const int MinimumHeight = 900;
+		private string? _settingsDirectory;
 
-//        //On Load
-//        public override async void _Ready()
-//        {
-//            _settingsDirectory = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "ToteschaMinecraft_Ender");
-//            _settingsController.LoadSettings(_settingsDirectory, out ToteschaSettings settings);
-//            ResizeWindow();
-//            await _webController.LoadServerDetailsAsync(settings.ServerURL);
+		private static string? _sceneName;
+		private static Node? _selectedNode;
 
-//            var thisWindow = GetWindow();
-//            thisWindow.CloseRequested += OnShutdown;
+		//On Load
+		public override async void _Ready()
+		{
+			_settingsDirectory = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "ToteschaMinecraft_Ender");
+			//_settingsController.LoadSettings(_settingsDirectory, out ToteschaSettings settings);
+			ResizeWindow();
+			//await _webController.LoadServerDetailsAsync(settings.ServerURL);
 
-//        }
-//        //On Shutdown
-//        private void OnShutdown()
-//        {
-//            _settingsController.SaveSettings(_settingsDirectory);
-//        }
+			var thisWindow = GetWindow();
+			thisWindow.CloseRequested += OnShutdown;
 
-//        //Lock window elements during web refreshes
-//        private void LockWindowElements(bool lockElements)
-//        {
-//            GetNode<Button>("/root/LauncherWindow/DisplayAreaContainer/MenuMargin/MenuContainer/SettingsButton").Disabled = lockElements;
-//            GetNode<Button>("/root/LauncherWindow/DisplayAreaContainer/MenuMargin/MenuContainer/DetailsButton").Disabled = lockElements;
-//            GetNode<Button>("/root/LauncherWindow/FooterContainer/LoginMargin/LoginButton").Disabled = lockElements;
-//        }
-//        //Update status bar
-//        private void UpdateStatusBar(string statusText, double loadingBarValue = 0.0)
-//        {
-//            GetNode<ProgressBar>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressBar").StopInfiniteLoading();
-//            GetNode<Label>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressLabel").Text = statusText;
+		}
+		//On Shutdown
+		private void OnShutdown()
+		{
+			//_settingsController.SaveSettings(_settingsDirectory);
+		}
 
-//            if (loadingBarValue > 1)
-//                GetNode<ProgressBar>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressBar").Value = loadingBarValue;
-//            else
-//                GetNode<ProgressBar>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressBar").StartInfiniteLoading();
-//        }
 
-//        //Handle Home Button Click
-//        //Handle Pack Details Click
-//        //Handle Settings Click
-//        //Handle Login Click
-//        //Handle Play Click
+		public void LockNodes(bool? lockAllNodes = null, bool? lockModpackNodes = null, bool? lockLoginNodes = null)
+		{
+			if (lockAllNodes!= null)
 
-//        //Handle Window Resize
-//        private void ResizeWindow()
-//        {
-//            OS.WindowSize = new Vector2(1280, 720);
-//        }
-//    }
-//}
+        }
+
+		private void LockAllNodes(bool lockElements)
+		{
+			GetNode<Button>("/root/LauncherWindow/DisplayAreaContainer/MenuMargin/MenuContainer/SettingsButton").Disabled = lockElements;
+			GetNode<Button>("/root/LauncherWindow/DisplayAreaContainer/MenuMargin/MenuContainer/DetailsButton").Disabled = lockElements;
+            GetNode<Button>("/root/LauncherWindow/DisplayAreaContainer/MenuMargin/MenuContainer/KeybindButton").Disabled = lockElements;
+            GetNode<Button>("/root/LauncherWindow/FooterContainer/LoginMargin/LoginButton").Disabled = lockElements;
+            GetNode<Button>("/root/LauncherWindow/FooterContainer/LaunchButtonContainer/LaunchButton").Disabled = lockElements;
+        }
+        private void LockModpackNodes(bool lockElements)
+        {
+            GetNode<Button>("/root/LauncherWindow/DisplayAreaContainer/MenuMargin/MenuContainer/DetailsButton").Disabled = lockElements;
+            GetNode<Button>("/root/LauncherWindow/DisplayAreaContainer/MenuMargin/MenuContainer/KeybindButton").Disabled = lockElements;
+            GetNode<Button>("/root/LauncherWindow/FooterContainer/LaunchButtonContainer/LaunchButton").Disabled = lockElements;
+        }
+        private void LockLoginNodes(bool lockElements)
+		{
+            GetNode<Button>("/root/LauncherWindow/FooterContainer/LoginMargin/LoginButton").Disabled = lockElements;
+            GetNode<Button>("/root/LauncherWindow/FooterContainer/LaunchButtonContainer/LaunchButton").Disabled = lockElements;
+        }
+		public void UpdateStatusBar(double loadingBarValue = 0.0)
+		{
+			GetNode<ProgressBar>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressBar").StopInfiniteLoading();
+
+			if (loadingBarValue >= 0)
+				GetNode<ProgressBar>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressBar").Value = loadingBarValue;
+			else
+				GetNode<ProgressBar>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressBar").StartInfiniteLoading();
+		}
+        public void UpdateStatusText(string statusText) => GetNode<Label>("/root/LauncherWindow/FooterContainer/MarginContainer/ProgressBarContainer/ProgressLabel").Text = statusText;
+
+
+
+        //Handle updating scene when a menu button is clicked
+        public void SetSceneInDisplayArea(PackedScene scene)
+		{
+			if (scene.ResourcePath != _sceneName)
+				_sceneName = scene.ResourcePath;
+			else
+				return;
+
+			if (_selectedNode != null)
+				_selectedNode.QueueFree();
+			_selectedNode = scene.Instantiate();
+
+			var displayArea = GetNode<MarginContainer>("/root/LauncherWindow/DisplayAreaContainer/MainMargin");
+			displayArea.AddChild(_selectedNode);
+		}
+
+		//Handle Window Resize
+		private void ResizeWindow()
+		{
+			var window = GetWindow();
+			window.ContentScaleSize = new Vector2I(MinimumWidth, MinimumHeight);
+
+
+			var displayDPI = DisplayServer.ScreenGetDpi();
+			var display = DisplayServer.GetDisplaySafeArea();
+			var developerDPI = 120;
+			var displaySizeOnStandardScreen = ((float)MinimumWidth / (float)developerDPI);
+
+
+			if (displayDPI > developerDPI)
+			{
+				var displaySizeOnPlayerScreen = ((float)MinimumWidth / (float)displayDPI);
+				var ratio = (1f - displaySizeOnPlayerScreen / displaySizeOnStandardScreen) + 1f;
+
+				var length = MinimumWidth * ratio;
+				var width = MinimumHeight * ratio;
+
+				//Resize the window
+				window.Size = new Vector2I((int)length, (int)width);
+
+				//Calculate the new position of the window. Center the window on the screen.
+				//The position is calculated by subtracting the window size from the display size and dividing by 2.
+				window.Position = new Vector2I((int)(display.Size.X - length) / 2, (int)(display.Size.Y - width) / 2);
+			}
+		}
+	}
+}
